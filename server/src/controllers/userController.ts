32e9370d8f92ffadce559050b480c1b580c34731
ihsan-hepsen <ihsanhepsen@gmail.com>
2023-08-10 23:46:48 -0500

    in-progress: adding update user function

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from 'express';
-import UserRepository from '../repositories/userRepository';
-import User from '../models/user';
+import { Request, Response } from 'express'
+import UserRepository from '../repositories/userRepository'
+import User from '../models/user'
+import Joi from 'joi'
 
 class UserController {
+
     async getAllUsers(req: Request, res: Response): Promise<void> {
         try {
-            let users: User[] = await UserRepository.getAllUsers();
-            res.json(users);
+            let users: User[] = await UserRepository.getAllUsers()
+            res.json(users)
         } catch (error) {
-            console.error('Error fetching users:', error);
-            res.status(500).json({ error: 'Internal server error' });
+            console.error('Error fetching users:', error)
+            res.status(500).json({ error: 'Internal server error' })
         }
     }
 
@@ -29,9 +31,44 @@ class UserController {
         }
     }
 
-    async deleteUser(req: Request, res: Response): Promise<void> {
+    async updateUser(req: Request, res: Response): Promise<void> {
+        console.log(`Received PUT request for user #${req.params.id}`)
         try {
-            console.log(`Received DELETE request for user #${req.params.id}`)
+            const userId = parseInt(req.params.id, 10)
+
+            const userSchema = Joi.object({
+                firstName: Joi.string().required(),
+                lastName: Joi.string().required(),
+                email: Joi.string().required(),
+                password: Joi.string().required(),
+            })
+
+            // const updatedUser: User = req.body as User
+            const { error, value: updatedUser } = userSchema.validate(req.body)
+
+            if (error) {
+                res.status(400).json({ error: error.details.map(detail => detail.message) })
+                return
+            }
+
+            const user = await UserRepository.updateUser(userId, updatedUser)
+
+            if (user) {
+                res.json(user)
+            } else if (!user) {
+                res.sendStatus(404).json({ error: 'User not found' })
+            } else {
+                res.sendStatus(400)
+            }
+        } catch (error) {
+            console.error('Error delete user by id:', error)
+            res.status(500).json({ error: 'Internal server error' })
+        }
+    }
+
+    async deleteUser(req: Request, res: Response): Promise<void> {
+        console.log(`Received DELETE request for user #${req.params.id}`)
+        try {
             const userId = parseInt(req.params.id, 10)
             const user = await UserRepository.deleteUser(userId)
 
@@ -41,10 +78,10 @@ class UserController {
                 res.sendStatus(404).json({ error: 'User not found' })
             }
         } catch (error) {
-            console.error('Error fetching user by id:', error)
+            console.error('Error delete user by id:', error)
             res.status(500).json({ error: 'Internal server error' })
         }
     }
 }
 
-export default new UserController();
+export default new UserController()
--- server/src/repositories/userRepository.ts
+++ server/src/repositories/userRepository.ts
@@ -1,9 +1,10 @@
 import e from 'express'
 import User from '../models/user'
 import pool from './db'
+import { number } from 'joi'
 
 class UserRepository {
-    async getAllUsers(): Promise<any[]> {
+    async getAllUsers(): Promise<User[]> {
         try {
             const [rows, fields] = await pool.execute('SELECT * FROM users') as [User[], any[]]
             return rows
@@ -21,16 +22,48 @@ class UserRepository {
         }
     }
 
-    // TODO: Add update function
+    async updateUser(id: number, updatedUser: User): Promise<User | null> {
+        try {
+            const [rows, fields] = await pool.execute('SELECT * FROM users WHERE id = ?', [id]) as [User[], any[]]
+            const oldUser = rows[0]
 
-    async deleteUser(id: number): Promise<User | null> {
+            if (!oldUser) {
+                return null
+            }
+
+            const updateQuery = `
+                UPDATE users
+                SET firstName = ?, lastName = ?, password = ?
+                WHERE id = ?
+            `
+            await pool.execute(updateQuery, [
+                updatedUser.firstName,
+                updatedUser.lastName,
+                updatedUser.password,
+                id
+            ])
+
+            oldUser.firstName = updatedUser.firstName
+            oldUser.lastName = updatedUser.lastName
+            oldUser.password = updatedUser.password
+
+            return oldUser
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async deleteUser(id: number): Promise<User> {
         try {
             const [rows, fields] = await pool.execute('SELECT * FROM users WHERE id = ?', [id]) as [User[], any[]]
             const user = rows[0]
-            if (user) {
-                await pool.execute('DELETE FROM users WHERE id = ?', [id])
+            if (!user) {
+                throw new Error(`User ${id} not found`)
             }
-            return user || null
+            await pool.execute('DELETE FROM users WHERE id = ?', [id])
+            return user
         } catch (error) {
             throw error
         }
